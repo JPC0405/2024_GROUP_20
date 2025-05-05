a11bb4e57c6bd0ae8d88b9569c00e048cfe3fbdc
Jacobm00re <eeyjm16@nottingham.ac.uk>
2025-05-05 17:18:28 +0000

    Deploying to gh-pages from @ JPC0405/2024_GROUP_20@70a230f076440612b267472f3b7d4646cdecc079 🚀

--- classMainWindow.js
+++ classMainWindow.js
@@ -4,6 +4,7 @@ var classMainWindow =
     [ "~MainWindow", "classMainWindow.html#ae98d00a93bc118200eeef9f9bba1dba7", null ],
     [ "handleButton", "classMainWindow.html#a132f0e713c1a2e43662407ad9858f933", null ],
     [ "handleTreeClick", "classMainWindow.html#a881c9312ae7d4cff66ff3b63f2616a62", null ],
+    [ "statusUpdateMessage", "classMainWindow.html#a86443ea744fda3e9bad328c2fd1c3d6b", null ],
     [ "updateChildren", "classMainWindow.html#a3010bbeebe63681b2cab64a583d16548", null ],
     [ "updateRender", "classMainWindow.html#aa2a5945f9c4bf90022f0a7ad3db34c49", null ],
     [ "UpdateRenderFromTree", "classMainWindow.html#a6b77a823210c660e9a2a7e693bae0e65", null ]
--- search/functions_b.js
+++ search/functions_b.js
@@ -3,5 +3,6 @@ var searchData=
   ['set_0',['set',['../classModelPart.html#a3232893d070ddcec42631bf5b043add8',1,'ModelPart']]],
   ['setcolour_1',['setColour',['../classModelPart.html#abbaaa19d7d24b9f1138072d594bd838c',1,'ModelPart']]],
   ['setname_2',['setName',['../classModelPart.html#a72b1a8a203f37095d0fff66d2e2b256e',1,'ModelPart']]],
-  ['setvisible_3',['setVisible',['../classModelPart.html#a96c8cca32bd530bf28c6ab6a1ccc2353',1,'ModelPart']]]
+  ['setvisible_3',['setVisible',['../classModelPart.html#a96c8cca32bd530bf28c6ab6a1ccc2353',1,'ModelPart']]],
+  ['statusupdatemessage_4',['statusUpdateMessage',['../classMainWindow.html#a86443ea744fda3e9bad328c2fd1c3d6b',1,'MainWindow']]]
 ];
